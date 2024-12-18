6b1d5afa971c95e3032869ff94ddb7c4ee34f885
Conor Stuart-Roe <conorstuartroe@gmail.com>
2024-12-17 21:35:23 -0500

    Tok Pisin + more functions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 import math
 import re
+import statistics
+
+from matplotlib import pyplot as plt
 
 
 @dataclass
@@ -44,7 +47,7 @@ LANGUAGES = [
         funny_words={"y": "i"},
         replacement_rules=[
             ("á", "a"), ("é", "e"), ("í", "i"), ("ó", "o"), ("ú", "u"),
-            ("v", "b"),
+            ("v", "b"), ("x", "gs"),
             ("que", "ke"), ("qui", "ki"),
             ("ge", "je"), ("gi", "ji"), ("gue", "ge"), ("gui", "gi"),
             ("ce", "ze"), ("ci", "zi"), ("ca", "ka"), ("co", "ko"), ("cu", "ku"),
@@ -69,6 +72,14 @@ LANGUAGES = [
         silent_segments=[],
     ),
     LanguageSpecification(
+        name="Tok Pisin",
+        iso="tpi",
+        funny_words={},
+        replacement_rules=[],
+        multigraphs=["ng", "ai", "au", "oi"],
+        silent_segments=[],
+    ),
+    LanguageSpecification(
         name="Toki Pona",
         iso="tok",
         funny_words={},
@@ -86,24 +97,30 @@ def next_segment(multigraphs: list[str], word: str, i: int):
     return word[i]
 
 
-def segment_words(lang: LanguageSpecification, sentence: str):
-    words = re.findall("[^\\W\\d_]+", sentence.lower())
-    words = [lang.funny_words.get(w, w) for w in words]
-    for i, w in enumerate(words):
-        for x, y in lang.replacement_rules:
-            w = w.replace(x, y)
-        words[i] = w
+def whitespace_words(sentence: str):
+    return re.findall("[^\\W\\d_]+", sentence.lower())
 
+
+def segment_word(lang: LanguageSpecification, word: str):
+    for x, y in lang.replacement_rules:
+        word = word.replace(x, y)
+
+    i = 0
+    segments = []
+    while i < len(word):
+        segment = next_segment(lang.multigraphs, word, i)
+        i += len(segment)
+        if segment not in lang.silent_segments:
+            segments.append(segment)
+
+    return segments
+
+
+def segment_words(lang: LanguageSpecification, sentence: str):
+    words = [lang.funny_words.get(w, w) for w in whitespace_words(sentence)]
     word_segments = []
-    for w in words:
-        i = 0
-        segments = []
-        while i < len(w):
-            segment = next_segment(lang.multigraphs, w, i)
-            i += len(segment)
-            if segment not in lang.silent_segments:
-                segments.append(segment)
-        word_segments.append(segments)
+    for word in words:
+        word_segments.append(segment_word(lang, word))
 
     return word_segments
 
@@ -124,6 +141,17 @@ def load_sentences(lang: LanguageSpecification):
     return sentences
 
 
+def all_segmented_words(lang: LanguageSpecification):
+    sentences = load_sentences(lang)
+    out: list[list[str]] = []
+    for s in sentences:
+        out += segment_words(lang, s)
+    return out
+
+
+EDGE_SEGMENT = "#"
+
+
 class SegmentSummary:
     def __init__(self):
         self.ngram_continuations: dict[tuple, dict[str, int]] = {}
@@ -137,41 +165,236 @@ class SegmentSummary:
                 self.ngram_continuations[start] = {}
             self.ngram_continuations[start][segment] = self.ngram_continuations[start].get(segment, 0) + 1
 
+    def add_all(self, segmented_words: list[list[str]], n: int):
+        for segments in segmented_words:
+            self.add(segments, n)
+
+    def segment_bytes(self, segment: str, context: list[str]):
+        d = self.ngram_continuations[tuple(context)]
+        odds = d[segment] / sum(d.values())
+        return -math.log2(odds)
+
     def bytes(self, segments: list[str], n: int):
-        segments_with_edge = ["#"]*n + segments + ["#"]
+        segments_with_edge = [EDGE_SEGMENT]*n + segments + [EDGE_SEGMENT]
         out = 0
         for i in range(len(segments)):
-            d = self.ngram_continuations[tuple(segments_with_edge[i:i+n])]
-            odds = d[segments_with_edge[i+n]]/sum(d.values())
-            out += -math.log2(odds)
+            out += self.segment_bytes(segments_with_edge[i+n], segments_with_edge[i:i+n])
         return out
 
 
-if __name__ == "__main__":
+DEFAULT_N = 2
+MAX_N = 8
+
+
+def show_basic_stats():
+    n = DEFAULT_N
     for lang in LANGUAGES:
-        # print(lang.name)
+        print(lang.name)
+
+        segmented_words = all_segmented_words(lang)
+        print(f"{len(segmented_words)} total words")
 
         sentences = load_sentences(lang)
-        all_segment_words: list[list[str]] = []
-        for s in sentences:
-            all_segment_words += segment_words(lang, s)
+        orthographic_words = set()
+        for sentence in sentences:
+            words = whitespace_words(sentence)
+            orthographic_words |= set(words)
+        print(f"{len(orthographic_words)} distinct orthographic words")
 
-        n = 3
+        num_segments = 0
+        all_segments = set()
+        for segments in segmented_words:
+            num_segments += len(segments)
+            for s in segments:
+                all_segments.add(s)
+        print(f"{num_segments} total segments")
+        print(f"{num_segments/len(segmented_words):.2f} average segments per word")
+        print(f"{len(all_segments)} distinct segments")
+
         ss = SegmentSummary()
-        for segments in all_segment_words:
-            ss.add(segments, n)
+        ss.add_all(segmented_words, n)
 
         total_bytes = 0
-        for segments in all_segment_words:
+        for segments in segmented_words:
             total_bytes += ss.bytes(segments, n)
-        print(lang.name, round(total_bytes))
+        print(f"{round(total_bytes)} bytes")
 
-        # sentence = sentences[0]
-        # print(sentence)
-        # sentence_bytes = 0
-        # for segments in segment_words(lang, sentence):
-        #     b = ss.bytes(segments, n)
-        #     print(segments, b)
-        #     sentence_bytes += b
-        # print(sentence_bytes)
-        # print()
+        print()
+
+
+def show_segmentation():
+    for lang in LANGUAGES:
+        sentences = load_sentences(lang)
+        segmented_words = segment_words(lang, sentences[0])
+        print(lang.name)
+        print(sentences[0])
+        print(segmented_words)
+        print()
+
+
+def show_bytes_per_segment():
+    data = [
+        (lang, [])
+        for lang in LANGUAGES
+    ]
+
+    for n in range(0, MAX_N):
+        for lang, bytes_per_segment in data:
+            print(lang.name)
+
+            segmented_words = all_segmented_words(lang)
+
+            num_segments = 0
+            segment_freqs = {}
+            for segments in segmented_words:
+                num_segments += len(segments)
+                for s in segments:
+                    segment_freqs[s] = segment_freqs.get(s, 0) + 1
+            print(f"{len(segment_freqs)} distinct segments")
+            for segment, count in sorted(list(segment_freqs.items()), key=lambda x: -x[1]):
+                print(segment, count)
+
+            ss = SegmentSummary()
+            ss.add_all(segmented_words, n)
+
+            total_bytes = 0
+            for segments in segmented_words:
+                total_bytes += ss.bytes(segments, n)
+
+            bytes_per_segment.append(total_bytes / num_segments)
+            print(f"{round(total_bytes / num_segments, 2)} bytes per segment")
+
+            print()
+        print('---')
+
+    for lang, bytes_per_segment in data:
+        plt.plot(*zip(*list(enumerate(bytes_per_segment))), label=lang.name)
+
+    plt.title("Average bytes per segment by n")
+    plt.legend()
+    plt.show()
+
+
+def show_total_bytes():
+    data = [
+        (lang, [])
+        for lang in LANGUAGES
+    ]
+
+    for n in range(0, MAX_N):
+        for lang, total_bytes_list in data:
+            segmented_words = all_segmented_words(lang)
+
+            ss = SegmentSummary()
+            ss.add_all(segmented_words, n)
+
+            total_bytes = 0
+            for segments in segmented_words:
+                total_bytes += ss.bytes(segments, n)
+            print(lang.name, round(total_bytes))
+            total_bytes_list.append(total_bytes)
+
+        print()
+
+    for lang, total_bytes_list in data:
+        plt.plot(*zip(*list(enumerate(total_bytes_list))), label=lang.name)
+
+    plt.title("Total bytes by n")
+    plt.legend()
+    plt.show()
+
+
+def show_first_segments():
+    n = 1
+    print("First segments")
+    print()
+    for lang in LANGUAGES:
+        print(lang.name)
+        segmented_words = all_segmented_words(lang)
+
+        ss = SegmentSummary()
+        ss.add_all(segmented_words, n)
+
+        left_edge = tuple([EDGE_SEGMENT]*n)
+        first_segments = ss.ngram_continuations[left_edge]
+        total_words = sum(first_segments.values())
+        expected_bytes = 0
+        for segment, times in sorted(list(first_segments.items()), key=lambda x: -x[1]):
+            odds = times/total_words
+            expected_bytes += -math.log2(odds)*odds
+            print(f"  {segment:<3} {times:>4}  {100*odds:.2f}%")
+        print(f"Expected bytes: {expected_bytes:.2f}")
+    print()
+
+
+def show_segment_position_bytes():
+    n = DEFAULT_N
+    max_position = 10
+    for lang in LANGUAGES:
+        segmented_words = all_segmented_words(lang)
+        ss = SegmentSummary()
+        ss.add_all(segmented_words, n)
+
+        position_bytes = []
+        for _ in range(max_position):
+            position_bytes.append([])
+
+        for word in segmented_words:
+            segments = [EDGE_SEGMENT]*n
+            for i, segment in enumerate(word):
+                if i >= max_position:
+                    break
+
+                context = segments[-n:] if n > 0 else ()
+                position_bytes[i].append(ss.segment_bytes(segment, context))
+                segments.append(segment)
+
+        avgs = []
+        for bs in position_bytes:
+            if not bs:
+                break
+            avgs.append(statistics.mean(bs))
+        plt.plot(*zip(*list(enumerate(avgs))), label=lang.name)
+
+    plt.title(f"Average bytes for segment at word position (n = {n})")
+    plt.legend()
+    plt.show()
+
+
+def show_most_common_words():
+    n = DEFAULT_N
+    for lang in LANGUAGES:
+        print(lang.name)
+        sentences = load_sentences(lang)
+        words = []
+        for sentence in sentences:
+            words += whitespace_words(sentence)
+        segmented_words = []
+        for word in words:
+            segmented_words.append(segment_word(lang, word))
+
+        ss = SegmentSummary()
+        ss.add_all(segmented_words, n)
+
+        word_freqs = {}
+        for word in words:
+            word_freqs[word] = word_freqs.get(word, 0) + 1
+
+        word_bytes = []
+        for word, count in sorted(list(word_freqs.items()), key=lambda x: -x[1])[:30]:
+            segments = segment_word(lang, word)
+            bs = ss.bytes(segments, n)
+            print(f"{word:<8} {count:>4} {bs:.2f}")
+            word_bytes.append(bs)
+
+        print()
+
+        plt.plot(*zip(*enumerate(word_bytes)), label=lang.name)
+
+    plt.title(f"Bytes of most common words (n = {n})")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    show_basic_stats()
